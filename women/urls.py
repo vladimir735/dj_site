fb209aa38f8cb9f53132694f2b5b342b6db1edb8
vladimir735 <mitin.3@mail.ru>
2023-05-07 21:53:26 +0300

    Приложение сайта о женщинах

---
+++ women/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns=[
+    path('', index, name='home'), #http://127.0.0.1:8000/
+    path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/cats/1
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category')
+]
